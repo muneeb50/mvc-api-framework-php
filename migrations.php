37e710d972c487b25d69c20abe17a999267d22f4
Muhammad Muneeb <40180015+muneeb50@users.noreply.github.com>
2021-12-09 06:26:09 +0500

    Update migrations.php

--- migrations.php
+++ migrations.php
@@ -6,7 +6,6 @@ use app\core\Application;
 require_once __DIR__.'/vendor/autoload.php';
 
 $config = [
-//    'userClass' => \app\models\User::class,
     'db' => [
         'dsn' => 'mysql:host=localhost;port=3306;dbname=mvc_api_framework_php',
         'user' => 'root',
